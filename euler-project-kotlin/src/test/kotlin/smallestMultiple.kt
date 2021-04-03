b4b54b4bf6f7c56f71f04e5e64b29bf67276f2f3
Johan Vergeer <johanvergeer@gmail.com>
2021-04-03 16:02:50 +0200

    Smallest multiple Kotlin optimised approach

--- euler-project-kotlin/src/test/kotlin/smallestMultiple.kt
+++ euler-project-kotlin/src/test/kotlin/smallestMultiple.kt
@@ -2,19 +2,42 @@ import assertk.assertThat
 import assertk.assertions.isEqualTo
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
+import kotlin.math.*
 
-class SmallestMultipleSpek: Spek({
+fun smallestMultipleNaiveApproach(): Int {
+    var i = 1
+    while (true) {
+        if ((2..20).all { i % it == 0 }) return i
+        i++
+    }
+}
+
+private fun exponent(prime: Int, limit: Float, of: Int): Int {
+    return if (prime <= limit) {
+        floor(log2(of.toFloat()) / log2(prime.toFloat())).toInt()
+    } else {
+        1
+    }
+}
+
+/**
+ * Optimized version based on https://projecteuler.net/overview=005
+ */
+fun smallestMultiple(of: Int): Int {
+    val limit = sqrt(of.toFloat())
+
+    return (listOf(1) + primesUpTo(of))
+        .reduce { multiple, prime ->
+            multiple * prime.pow(exponent(prime, limit, of))
+        }
+}
+
+class SmallestMultipleSpek : Spek({
     describe("when searching for the smallest number evenly divisible by all of the numbers from 1 to 20") {
         it("will be 232792560") {
             val expected = 232792560
 
-            var i = 1
-            while (true){
-                if ((2..20).all { i % it == 0 }) break
-                i ++
-            }
-
-            assertThat(i).isEqualTo(expected)
+            assertThat(smallestMultiple(20)).isEqualTo(expected)
         }
     }
 })
